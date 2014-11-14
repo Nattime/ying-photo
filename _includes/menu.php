315f3dbd73f67d45daf8138e17333c1189cd6291
Jie <Nattime@gmail.com>
2014-11-14 12:21:00 -0500

    modularize php

---
+++ _includes/menu.php
@@ -0,0 +1,11 @@
+<div class="menu-container">
+    <ul class="menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about_me.php">About Me</a></li>
+        <li><a href="fine_arts.php">Fine Arts</a></li>
+        <li><a href="corp_events.php">Corporate Events</a></li>
+        <li><a href="weddings.php">Weddings</a></li>
+        <li><a href="photo_lessons.php">Photo Lessons</a></li>
+        <li><a href="contacts.php">Contact</a></li>
+    </ul>
+</div><!-- end menu-container -->
--- index.php
+++ index.php
@@ -5,35 +5,26 @@
 		<meta name="description" content="">
 		<meta name="keywords" content="">
 		<link href="_css/main_styles.css" type="text/css" rel="stylesheet">
-		
+
 	</head>
 	<body>
 		<header>
 			<a href="index.html">
 				<img src="./_img/logo.gif">
 			</a>
-			
-			<div class="menu-container">
-				<ul class="menu">
-					<li><a href="index.html">Home</a></li>
-					<li><a href="about_me.html">About Me</a></li>
-					<li><a href="fine_arts.html">Fine Arts</a></li>
-					<li><a href="corp_events.html">Corporate Events</a></li>
-					<li><a href="weddings.html">Weddings</a></li>
-					<li><a href="photo_lessons.html">Photo Lessons</a></li>
-					<li><a href="contacts.html">Contact</a></li>
-				</ul>
-			</div><!-- end menu-container -->
+
+			<?php include './_includes/menu.php' ?>
+
 		</header>
-		
+
 		<div class="main-container">
-			
+
 			<ul class="img-slider">
 				<li><img src="_img/img_1.JPG" alt=""></li>
 				<li><img src="_img/img_2.JPG" alt=""></li>
 				<li><img src="_img/img_3.JPG" alt=""></li>
 			</ul>
-			
+
 		</div><!-- end container -->
 	</body>
 </html>
